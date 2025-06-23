f063916c0397be2b9944db081ab8b0cee284ce9f
Beau McCartney <mccartney.beausl@gmail.com>
2025-06-22 18:19:34 -0600

    remove unused post-build hook

--- hooks/post-build.lua
+++
@@ -1,4 +0,0 @@
-local highlight_css = Sys.read_file("node_modules/highlight.js/styles/base16/gruvbox-dark-medium.min.css")
-if not highlight_css then
-	Plugin.fail("syntax highlighting stylesheet not found")
-end
