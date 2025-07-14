16eecb3dc8b814d39fbd10aea8850c3516525238
Beau McCartney <mccartney.beausl@gmail.com>
2025-07-14 11:54:37 -0600

    use regular template strings instead of handlebars

--- generate.ts
+++ generate.ts
@@ -16,8 +16,6 @@ import * as cheerio from "cheerio";
 
 import hljs from "highlight.js/lib/common";
 
-import Handlebars from "handlebars";
-
 import { $ } from "bun";
 
 process.chdir(import.meta.dir);
@@ -45,21 +43,14 @@ const katex_module = path.join(node_modules_dir, "katex/dist");
 await $`cp ${path.join(katex_module, "katex.min.css")} ${path.join(css_out_dir, katex_css_out)}`;
 await $`cp ${path.join(katex_module, "fonts")}/* ${fonts_out_dir}`;
 
-const page_content_template = Handlebars.compile(`{{{content}}}
-{{#if footnotes}}
-<hr/>
-<ol style="list-style-type: none; padding-left: 0;">
-  {{#each footnotes}}
-  <li id="{{> footnote_id_template ix=@index}}"><p><sup><a href="#{{> footnote_link_id_template ix=@index}}">{{footnote_number @index}}.</a></sup> {{{this}}}</p></li>
-  {{/each}}
-</ol>
-{{/if}}`);
-
-const complete_page_template = Handlebars.compile(`<!DOCTYPE html>
+const complete_page_template = (
+  title: string,
+  content: string,
+) => `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>{{title}}</title>
+    <title>${title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <meta name="author" content="Beau McCartney">
@@ -70,46 +61,36 @@ const complete_page_template = Handlebars.compile(`<!DOCTYPE html>
     >
     <meta name="robots" content="index, follow">
 
-    <link rel="stylesheet" href="/{{css_dir}}/{{katex_css_out}}">
+    <link rel="stylesheet" href="${path.join("/", css_dir, katex_css_out)}">
 
-    <link rel="stylesheet" href="/{{css_dir}}/sakura-dark.css" media="screen and (prefers-color-scheme: dark)">
+    <link rel="stylesheet" href="${path.join("/", css_dir, "sakura-dark.css")}" media="screen and (prefers-color-scheme: dark)">
 
-    <link rel="stylesheet" href="/{{css_dir}}/{{highlight_css_out}}">
+    <link rel="stylesheet" href="${path.join("/", css_dir, highlight_css_out)}">
 
-    <link rel="alternate" type="application/rss+xml" title="RSS" href="/{{rss_file}}">
+    <link rel="alternate" type="application/rss+xml" title="RSS" href="/${rss_file}">
     <script data-goatcounter="https://beaumccartney.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
   </head>
   <body>
     <main>
-    {{{content}}}
+    ${content}
     </main>
   </body>
-</html>`);
+</html>`;
 
 // helper for computing the footnote number in the template
 function footnote_number(ix: number): number {
   return ix + 1;
 }
-Handlebars.registerHelper("footnote_number", footnote_number);
 
-const footnote_link_id_template = Handlebars.compile("fn_link-{{ix}}");
-Handlebars.registerPartial(
-  "footnote_link_id_template",
-  footnote_link_id_template,
-);
+const footnote_link_id_template = (ix: number) => `fn_link-${ix}`;
 
-const footnote_id_template = Handlebars.compile("fn-{{ix}}");
-Handlebars.registerPartial("footnote_id_template", footnote_id_template);
+const footnote_id_template = (ix: number) => `fn-${ix}`;
 
-const time_element_template = Handlebars.compile(
-  '<time datetime="{{thedate}}">{{thedate}}</time>',
-);
-Handlebars.registerPartial("time_element_template", time_element_template);
+const time_element_template = (thedate: string) =>
+  `<time datetime="${thedate}">${thedate}</time>`;
 
-const footnote_link_template = Handlebars.compile(
-  '<sup id="{{> footnote_link_id_template}}"><a href="#{{> footnote_id_template }}">{{footnote_number ix}}</a></sup>',
-);
-
+const footnote_link_template = (ix: number) =>
+  `<sup id="${footnote_link_id_template(ix)}"><a href="#${footnote_id_template(ix)}">${footnote_number(ix)}</a></sup>`;
 async function process_markdown_content(file: Bun.BunFile) {
   const markdown = await file.bytes();
   const converted_html = micromark(markdown, {
@@ -145,13 +126,26 @@ async function process_markdown_content(file: Bun.BunFile) {
     .map(function (ix) {
       const $footnote = $page(this);
       const footnote = $footnote.html();
-      $footnote.replaceWith(footnote_link_template({ ix }));
+      $footnote.replaceWith(footnote_link_template(ix));
       return footnote;
     })
     .toArray();
 
+  let content = $page.html();
+  if (footnotes.length) {
+    content += `
+<hr/>
+<ol style="list-style-type: none; padding-left: 0;">
+  ${footnotes
+    .map(
+      (note, ix) =>
+        `<li id="${footnote_id_template(ix)}"><p><sup><a href="#${footnote_link_id_template(ix)}">${footnote_number(ix)}.</a></sup> ${note}</p></li>`,
+    )
+    .join("\n")}
+</ol>`;
+  }
   return {
-    content: page_content_template({ footnotes, content: $page.html() }),
+    content,
     title: $page("h1").text(),
   };
 }
@@ -178,7 +172,7 @@ for await (const dirent of await opendir(path.join(src_dir, blog_dir))) {
   const publish_date = $pubdate.text();
   if (publish_date.length !== 0) {
     $pubdate.replaceWith(
-      `<strong>${time_element_template({ thedate: publish_date })}</strong>`,
+      `<strong>${time_element_template(publish_date)}</strong>`,
     );
   }
 
@@ -193,14 +187,7 @@ for await (const dirent of await opendir(path.join(src_dir, blog_dir))) {
   };
   posts.push(post);
 
-  const post_page = complete_page_template({
-    title: post.title,
-    content: post.html,
-    css_dir,
-    katex_css_out,
-    highlight_css_out,
-    rss_file,
-  });
+  const post_page = complete_page_template(post.title, post.html);
 
   Bun.write(path.join(build_dir, post.folder, "index.html"), post_page);
 }
@@ -209,61 +196,45 @@ for await (const dirent of await opendir(path.join(src_dir, blog_dir))) {
   const home = await process_markdown_content(
     Bun.file(path.join(src_dir, `index${markdown_extension}`)),
   );
-  const blog_toc_template = Handlebars.compile(`<ul class="unstyled-ol">
-  {{#each posts}}
-  <li>{{> time_element_template thedate=this.publish_date }} - <a href="/{{ this.folder }}">{{ this.title }}</a></li>
-  {{/each}}
-</ul>
-<a href="/rss.xml">RSS Feed</a>`);
   posts.sort((a, b) => b.publish_date.localeCompare(a.publish_date));
   const $home = cheerio.load(home.content, {}, false);
-  $home("#blog-entries").html(blog_toc_template({ posts }));
+  $home("#blog-entries").html(`<ul>
+  ${posts.map((post) => `<li>${time_element_template(post.publish_date)} - <a href="${path.join("/", post.folder)}">${post.title}</a></li>`).join("\n")}
+</ul>
+<a href="/rss.xml">RSS Feed</a>`);
 
-  const home_page = complete_page_template({
-    title: home.title,
-    content: $home.html(),
-    css_dir,
-    katex_css_out,
-    highlight_css_out,
-    rss_file,
-  });
+  const home_page = complete_page_template(home.title, $home.html());
 
   Bun.write(path.join(build_dir, "index.html"), home_page);
 }
 
-const rss_template = Handlebars.compile(`<?xml version="1.0" encoding="UTF-8" ?>
+const website_url = "https://beaumccartney.com";
+const rss_feed = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 
 <channel>
   <title>Beau McCartney</title>
-  <link>{{ website_url }}</link>
+  <link>${website_url}</link>
   <description>Beau McCartney's website and blog</description>
   <language>en-us</language>
-  <atom:link href="{{ website_url }}/rss.xml" rel="self" type="application/rss+xml" />
-  {{#each rss_posts}}
-  <item>
-    <title>{{this.title}}</title>
-    <link>{{this.web_url}}</link>
-    <guid>{{this.web_url}}</guid>
-    <pubDate>{{this.rss_publish_date}}</pubDate>
-    <description><![CDATA[{{{this.html}}}]]></description>
-  </item>
-  {{/each}}
+  <atom:link href="${website_url}/rss.xml" rel="self" type="application/rss+xml" />
+  ${posts
+    .map((post) => {
+      const date = new Date(post.publish_date);
+      const rss_publish_date = date.toUTCString();
+      const web_url = path.join(website_url, post.folder);
+
+      return `<item>
+  <title>${post.title}</title>
+  <link>${web_url}</link>
+  <guid>${web_url}</guid>
+  <pubDate>${rss_publish_date}</pubDate>
+  <description><![CDATA[${post.html}]]></description>
+</item>
+    `;
+    })
+    .join("\n")}
 </channel>
+</rss>`;
 
-</rss>`);
-
-const website_url = "https://beaumccartney.com"
-const rss_posts = posts.map((post) => {
-  const date = new Date(post.publish_date);
-  const rss_publish_date = date.toUTCString();
-
-  return {
-    title: post.title,
-    web_url: path.join(website_url, post.folder),
-    rss_publish_date,
-    html: post.html,
-  };
-});
-const rss_feed = rss_template({ website_url, rss_posts });
 Bun.write(path.join(build_dir, rss_file), rss_feed);
